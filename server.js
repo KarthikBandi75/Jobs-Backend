eda1423f6562553d700958b8f8a0fd743340fb83
Bandi Karthik <bandikarthik75@gmail.com>
2025-04-24 01:12:55 +0530

    cors enabled in server.js for frontend urls

--- server.js
+++ server.js
@@ -12,16 +12,22 @@ import applicationRoutes from './routes/applicationRoutes.js';
 dotenv.config();
 const app = express();
 
+app.use(cors({
+  origin: [
+    'https://jobs-admin-employee-frontend.vercel.app',
+    'https://jobs-user-frontend.vercel.app'
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.use('/api/admin', adminRoutes);
 app.use('/api/employee', employRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/applications', applicationRoutes);
-
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -44,3 +50,5 @@ const startServer = async () => {
 };
 
 startServer();
+
+
